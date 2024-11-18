c0f0826005957b2bf28c65f9ad99d064b2d1be05
Krystian Garcia <Krystian.Garcia@Gmail.com>
2024-11-18 03:14:18 -0700

    Added Sleep game loop

--- TestGame/src/Test_Package/GameScreen.java
+++ TestGame/src/Test_Package/GameScreen.java
@@ -1,30 +1,37 @@
 package Test_Package;
 
 import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
+//import java.awt.Dimension;
 
 public class GameScreen extends JPanel implements Runnable{
 
-//    final int originalTileSize = 16;
-//    final int scale = 3;
-//
-//    final int tileSize = originalTileSize * scale;
-//    final int maxScrnCol = 16;
-//    final int maxScrnRow = 12;
-//
-//    final int scrnWidth = tileSize * maxScrnCol;
-//    final int scrnHeight = tileSize * maxScrnRow;
-    final int scrnWidth = 800;
-    final int scrnHeight = 600;
+    final int originalTileSize = 16;
+    final int scale = 3;
 
+    final int tileSize = originalTileSize * scale;
+    final int maxScrnCol = 16;
+    final int maxScrnRow = 12;
+
+    final int scrnWidth = tileSize * maxScrnCol;
+    final int scrnHeight = tileSize * maxScrnRow;
+
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gamethread;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GameScreen(){
 
         this.setPreferredSize(new Dimension(scrnWidth, scrnHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
 
     }
 
@@ -33,10 +40,65 @@ public class GameScreen extends JPanel implements Runnable{
         gamethread.start();
     }
 
-//    @Override
+    @Override
     public void run() {
-//        while (gamethread != null) {
-//            System.out.println("app is running");
-//        }
+
+        double drawInterval = 1000000000/FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while (gamethread != null) {
+
+            update();
+
+            repaint();
+
+
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+
+                if (remainingTime < 0){
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long)remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+
+        }
+
     }
+
+    public void update(){
+        if (keyH.upPressed == true){
+            playerY -= playerSpeed;
+        }else if (keyH.downPressed == true){
+            playerY += playerSpeed;
+        }else if (keyH.leftPressed == true){
+            playerX -= playerSpeed;
+        }else if (keyH.rightPressed == true){
+            playerX += playerSpeed;
+        }
+
+    }
+
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX,playerY,tileSize,tileSize);
+
+        g2.dispose();
+    }
+
 }
