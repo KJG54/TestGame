93566176bb526db733cd6a2890a292090df6436f
Krystian Garcia <Krystian.Garcia@Gmail.com>
2024-11-18 00:23:35 -0700

    changed height and width

--- TestGame/src/Test_Package/GameScreen.java
+++ TestGame/src/Test_Package/GameScreen.java
@@ -6,15 +6,17 @@ import java.awt.Dimension;
 
 public class GameScreen extends JPanel implements Runnable{
 
-    final int originalTileSize = 16;
-    final int scale = 3;
-
-    final int tileSize = originalTileSize * scale;
-    final int maxScrnCol = 16;
-    final int maxScrnRow = 12;
-
-    final int scrnWidth = tileSize * maxScrnCol;
-    final int scrnHeight = tileSize * maxScrnRow;
+//    final int originalTileSize = 16;
+//    final int scale = 3;
+//
+//    final int tileSize = originalTileSize * scale;
+//    final int maxScrnCol = 16;
+//    final int maxScrnRow = 12;
+//
+//    final int scrnWidth = tileSize * maxScrnCol;
+//    final int scrnHeight = tileSize * maxScrnRow;
+    final int scrnWidth = 800;
+    final int scrnHeight = 600;
 
     Thread gamethread;
 
@@ -27,7 +29,7 @@ public class GameScreen extends JPanel implements Runnable{
     }
 
     public void startGameThread(){
-        gamethread = new Thread(this);
+        gamethread = new Thread(gamethread);
         gamethread.start();
     }
 
